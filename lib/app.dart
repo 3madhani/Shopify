4236bb0c0117c44ee720cde4d64be905e22cfc61
3mad Hany <144627820+3madhani@users.noreply.github.com>
2024-11-12 21:13:48 +0200

    create native splash screen

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'core/utils/theme/theme.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      
+    );
+  }
+}
---
+++ lib/core/utils/constants/image_string.dart
@@ -0,0 +1,10 @@
+class AppImages{
+
+  // app logo
+  static const String darkAppLogo = 'assets/logos/light_logo.svg'; // dark appLogo
+  static const String lightAppLogo = 'assets/logos/dark_logo.svg'; // light appLogo
+
+  // social logo
+  static const String googleLogo = 'assets/logos/google.svg';
+  static const String facebookLogo = 'assets/logos/facebook.svg';
+}
