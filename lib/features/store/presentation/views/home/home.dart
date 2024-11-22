68ff13fc80f15236ac89ada34d1454cbf3d2c478
3mad Hany <144627820+3madhani@users.noreply.github.com>
2024-11-22 13:12:07 +0200

    create circular container

--- lib/features/store/presentation/views/home/home.dart
+++ lib/features/store/presentation/views/home/home.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/core/utils/constants/colors.dart';
+
+import '../../../../../core/common/widgets/custom_shapes/containers/circular_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: AppColors.primary,
+              padding: const EdgeInsets.all(0),
+              child: Stack(
+                children: [
+                  CircularContainer(
+                    backgroundColor: AppColors.textWhite.withOpacity(0.1),
+                  ),
+                   CircularContainer(
+                    backgroundColor: AppColors.textWhite.withOpacity(0.1),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
