65f47dac06329dfffe6ad79e0cbafec01cea9def
3mad Hany <144627820+3madhani@users.noreply.github.com>
2024-11-27 01:19:35 +0200

    extract home AppBar

--- lib/features/store/presentation/views/home/home.dart
+++ lib/features/store/presentation/views/home/home.dart
@@ -1,48 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:t_store/core/common/widgets/app_bar/app_bar.dart';
-import 'package:t_store/core/utils/constants/text_string.dart';
 import '../../../../../core/common/widgets/custom_shapes/containers/primary_header_container.dart';
-import '../../../../../core/common/widgets/products_cart/cart_menu_icon.dart';
-import '../../../../../core/utils/constants/colors.dart';
+import 'widgets/home_app_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             PrimaryHeaderContainer(
               child: Column(
                 children: [
-                  AppAppBar(
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          AppTexts.homeAppBarTitle,
-                          style: Theme.of(context).textTheme.labelMedium!.apply(
-                                color: AppColors.grey,
-                              ),
-                        ),
-                        Text(
-                          AppTexts.homeAppBarSubTitle,
-                          style:
-                              Theme.of(context).textTheme.headlineSmall!.apply(
-                                    color: AppColors.white,
-                                  ),
-                        )
-                      ],
-                    ),
-                    actions: [
-                      CartCounterIcon(
-                        onPressed: () {},
-                        iconColor: AppColors.white,
-                      ),
-                    ],
-                  ),
+                  // AppBar
+                  HomeAppBar(),
+                  // SearchBar
+
+                  // Categories
+                  
                 ],
               ),
             ),
@@ -52,4 +29,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
