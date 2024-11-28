d969998693a25898850b08dff8a15983484a21a6
3mad Hany <144627820+3madhani@users.noreply.github.com>
2024-11-28 18:01:13 +0200

    create show case widget

--- lib/features/store/presentation/views/store/store.dart
+++ lib/features/store/presentation/views/store/store.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/core/common/widgets/app_bar/app_bar.dart';
 import 'package:t_store/core/common/widgets/app_bar/tab_bar.dart';
-import 'package:t_store/core/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:t_store/core/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:t_store/core/common/widgets/layouts/grid_layout.dart';
 import 'package:t_store/core/common/widgets/products/cart/cart_menu_icon.dart';
 import 'package:t_store/core/common/widgets/texts/section_heading.dart';
+import 'package:t_store/core/utils/constants/image_string.dart';
 import 'package:t_store/core/utils/constants/sizes.dart';
-import '../../../../../core/common/widgets/products/product_cards/brand_card.dart';
+import '../../../../../core/common/widgets/brands/brand_card.dart';
+import '../../../../../core/common/widgets/brands/brand_show_cases.dart';
 import '../../../../../core/utils/constants/colors.dart';
 import '../../../../../core/utils/helpers/helper_functions.dart';
 
@@ -105,25 +106,63 @@ class StoreScreen extends StatelessWidget {
             children: [
               Padding(
                 padding: EdgeInsets.all(AppSizes.defaultSpace),
-                child:Column(
-                  children: [
-                    RoundedContainer(
-                      shadowBorder: true,
-                      borderColor: AppColors.darkGrey,
-                      backgroundColor: Colors.transparent,
-                      margin: EdgeInsets.only(
-                        bottom: AppSizes.spaceBtwItems,
-                      ),
-                      child: Column(
-                        children: [
-
-                          // brand card
-                          BrandCard(showBorder: false,),
-                        ],
-                      ),
-                    )
-                  ],
-                ) ,
+                child: Column(children: [
+                  BrandShowCases(
+                    images: [
+                      AppImages.product1,
+                      AppImages.product2,
+                      AppImages.product3
+                    ],
+                  ),
+                ]),
+              ),
+              Padding(
+                padding: EdgeInsets.all(AppSizes.defaultSpace),
+                child: Column(children: [
+                  BrandShowCases(
+                    images: [
+                      AppImages.product1,
+                      AppImages.product2,
+                      AppImages.product3
+                    ],
+                  ),
+                ]),
+              ),
+              Padding(
+                padding: EdgeInsets.all(AppSizes.defaultSpace),
+                child: Column(children: [
+                  BrandShowCases(
+                    images: [
+                      AppImages.product1,
+                      AppImages.product2,
+                      AppImages.product3
+                    ],
+                  ),
+                ]),
+              ),
+              Padding(
+                padding: EdgeInsets.all(AppSizes.defaultSpace),
+                child: Column(children: [
+                  BrandShowCases(
+                    images: [
+                      AppImages.product1,
+                      AppImages.product2,
+                      AppImages.product3
+                    ],
+                  ),
+                ]),
+              ),
+              Padding(
+                padding: EdgeInsets.all(AppSizes.defaultSpace),
+                child: Column(children: [
+                  BrandShowCases(
+                    images: [
+                      AppImages.product1,
+                      AppImages.product2,
+                      AppImages.product3
+                    ],
+                  ),
+                ]),
               ),
             ],
           ),
